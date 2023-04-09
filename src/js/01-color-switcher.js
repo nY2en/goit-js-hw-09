8c1776fdca61021e9ad9110b56a773f48ddccc8a
Nikita Chebanenko <chebanikita@icloud.com>
2023-04-09 16:18:05 +0300

    firstTask

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+const DELAY = 1000;
+let intervalId = null;
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  refs.stopBtn.disabled = false;
+  refs.startBtn.disabled = true;
+
+  intervalId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, DELAY);
+}
+
+function onStopBtnClick() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
